ec488c13db87def2cdbaaabee3b062822797e776
773763165 <108125813+773763165@users.noreply.github.com>
2023-07-21 16:58:38 +0800

    config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = 'olozhika'
+USERNAME = 'alzn'
 
 # The repository to add this issue to
-REPO_OWNER = 'olozhika'
+REPO_OWNER = 'alzn'
 REPO_NAME = 'ArXivDaily_StarFormation'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["star formation", "molecular cloud", "interstellar medium", "cloud", "clump", "core", "filament", "atomic gas", "N-PDF"]
+KEYWORD_LIST = ["star formation", "molecular cloud", "interstellar medium", "cloud", "clump", "core", "filament", "atomic gas", "N-PDF", "bubble", "shell", "HI"]
 # Keywords to exclude
 KEYWORD_EX_LIST = ["galaxies", "galaxy cluster", "AGN"]
