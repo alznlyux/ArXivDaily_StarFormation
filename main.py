caf97ef62a4b4f2a5d301d3e30d55506364a1419
AlZn <108125813+alznlyux@users.noreply.github.com>
2025-10-20 23:28:19 +0800

    Update author name in full_report string

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def main(args):
         full_report = full_report + report + '\n'
         
     full_report = full_report + '\n\n'
-    full_report = full_report + 'by olozhika (Xing Yuchen). ' + '\n'
+    full_report = full_report + 'by AlZn (Xin Lyu). ' + '\n'
     full_report = full_report + '\n\n'
     full_report = full_report + datetime.datetime.now().strftime("%Y-%m-%d") + '\n'
 
